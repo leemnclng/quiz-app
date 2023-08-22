20b3f769c90f34f7f4207c447150351f23cc7740
Lee Michael Maneclang <lmaneclang@highlysucceed.com>
2023-08-22 20:20:45 +0800

    Delete Register Component

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -11,14 +11,12 @@ import {
 // importing components for the elements of the path
 import Homepage from './Pages/Homepage';
 import LoginComp from './Pages/AuthPage/Login';
-import RegisterComp from './Pages/AuthPage/Register';
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<Homepage />}>
             <Route path='auth'>
                 <Route path='login' element={<LoginComp />}/>
-                <Route path='register' element={<RegisterComp />}/>
             </Route>
         </Route >
     )
