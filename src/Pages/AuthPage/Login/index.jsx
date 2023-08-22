e073b6f15082f0b5b1c943b82a723e0ead728c56
Lee Michael Maneclang <lmaneclang@highlysucceed.com>
2023-08-22 20:17:56 +0800

    add pages for authentication

---
+++ src/Pages/AuthPage/Login/index.jsx
@@ -0,0 +1,10 @@
+// import React from "react";
+
+const LoginComp = () => {
+    return (
+        <h1>This is the Login Page</h1>
+    )
+}
+
+export default LoginComp;
+
---
+++ src/Pages/AuthPage/Register/index.jsx
@@ -0,0 +1,10 @@
+// import React from "react";
+
+const RegisterComp = () => {
+    return (
+        <h1>This is the Register Page</h1>
+    )
+}
+
+export default RegisterComp;
+
